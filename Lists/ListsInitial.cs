fdf3333b01b0f2b9357239b824bf799c3f329ede
Neven Dimač <neven.dimac@gmail.com>
2022-06-30 23:44:12 +0200

    Added listst initial

---
+++ Lists/ListsInitial.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lists
+{
+    public class ListsInitial
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
