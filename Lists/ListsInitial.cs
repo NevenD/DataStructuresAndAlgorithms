2f65a62eec5580eb44f767a55515210d8c7139fb
Neven Dimač <neven.dimac@gmail.com>
2022-07-06 00:25:37 +0200

    Added node logic

--- Lists/ListsInitial.cs
+++ Lists/ListsInitial.cs
@@ -7,9 +7,28 @@ namespace Lists
     {
         private static void Main(string[] args)
         {
-            RunList();
-
+            //RunList();
+            ChainedNodes();
             Console.Read();
+        }
+
+        public static void ChainedNodes()
+        {
+            Node firstNode = new Node() { Value = 5 };
+            Node secondNode = new Node() { Value = 1 };
+
+            firstNode.Next = secondNode;
+
+            Node thirdNode = new Node() { Value = 3 };
+            secondNode.Next = thirdNode;
+
+
+
+            while (firstNode != null)
+            {
+                Console.WriteLine(firstNode.Value);
+                firstNode = firstNode.Next;
+            }
         }
 
         // Resizing is an expensive operation
---
+++ Lists/Node.cs
@@ -0,0 +1,13 @@
+﻿namespace Lists
+{
+
+    // recursive data structure
+    // contains an reference to an object 
+    // of its own type
+    public class Node
+    {
+        public int Value { get; set; }
+        public Node Next { get; set; }
+
+    }
+}
