7a0f58349f566714a4189e8b02c0e7319d293f24
Neven Dimač <neven.dimac@gmail.com>
2022-07-06 23:36:18 +0200

    Added generic type

--- Lists/Node.cs
+++ Lists/Node.cs
@@ -4,10 +4,15 @@
     // recursive data structure
     // contains an reference to an object 
     // of its own type
-    public class Node
+    public class Node<T>
     {
-        public int Value { get; set; }
-        public Node Next { get; set; }
+        public T Value { get; set; }
+        public Node<T> Next { get; set; }
+
+        public Node(T value)
+        {
+            Value = value;
+        }
 
     }
 }
