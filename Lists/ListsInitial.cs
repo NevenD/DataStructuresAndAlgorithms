07df2dcf30afcd698db5dbad11fd90e157f3de09
Neven Dimač <neven.dimac@gmail.com>
2022-07-06 23:32:07 +0200

    Add linked list helpers

--- Lists/ListsInitial.cs
+++ Lists/ListsInitial.cs
@@ -7,28 +7,10 @@ namespace Lists
     {
         private static void Main(string[] args)
         {
-            //RunList();
-            ChainedNodes();
+            ListsInitialHelpers.
+                        //RunList();
+                        ChainedNodes();
             Console.Read();
-        }
-
-        public static void ChainedNodes()
-        {
-            Node firstNode = new Node() { Value = 5 };
-            Node secondNode = new Node() { Value = 1 };
-
-            firstNode.Next = secondNode;
-
-            Node thirdNode = new Node() { Value = 3 };
-            secondNode.Next = thirdNode;
-
-
-
-            while (firstNode != null)
-            {
-                Console.WriteLine(firstNode.Value);
-                firstNode = firstNode.Next;
-            }
         }
 
         // Resizing is an expensive operation
---
+++ Lists/ListsInitialHelpers.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Lists
+{
+    public static class ListsInitialHelpers
+    {
+
+        public static void ChainedNodes()
+        {
+            Node firstNode = new Node() { Value = 5 };
+            Node secondNode = new Node() { Value = 1 };
+
+            firstNode.Next = secondNode;
+
+            Node thirdNode = new Node() { Value = 3 };
+            secondNode.Next = thirdNode;
+
+
+
+            while (firstNode != null)
+            {
+                Console.WriteLine(firstNode.Value);
+                firstNode = firstNode.Next;
+            }
+        }
+    }
+}
