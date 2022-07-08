aa116e9d467ee0d671c16627aa5293d17a355676
Neven Dimač <neven.dimac@gmail.com>
2022-07-08 23:59:47 +0200

    Added and completed array stack logic and unit test Initi linked stack

---
+++ Stacks/ArrayStack.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Stacks
+{
+    public class ArrayStack<T> : IEnumerable<T>
+    {
+
+        private T[] _items;
+
+        public ArrayStack()
+        {
+            const int defaultCapacity = 4;
+            _items = new T[defaultCapacity];
+        }
+
+        public ArrayStack(int capacity)
+        {
+            _items = new T[capacity];
+        }
+
+
+        public void Push(T item)
+        {
+            if (_items.Length == Count)
+            {
+                T[] doubleArray = new T[Count * 2];
+                Array.Copy(_items, doubleArray, Count);
+
+                _items = doubleArray;
+            }
+            _items[Count] = item;
+            Count++;
+        }
+
+        public T Peek()
+        {
+            return _items[Count - 1];
+        }
+
+        public void Pop()
+        {
+            if (IsEmpty)
+            {
+                throw new InvalidOperationException();
+            }
+
+            Count--;
+            _items[Count] = default(T);
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = Count - 1; i >= 0; i--)
+            {
+                yield return _items[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public int Count { get; private set; }
+
+        public bool IsEmpty => Count == 0;
+
+    }
+}
---
+++ Stacks/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Stacks
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            var stacks = new ArrayStack<int>();
+            stacks.Push(1);
+            stacks.Push(2);
+            stacks.Push(3);
+            stacks.Push(4);
+
+
+            Console.WriteLine($"Should write out 4: {stacks.Peek()}");
+
+            stacks.Pop();
+
+            Console.WriteLine($"Should write out 3: {stacks.Peek()}");
+
+            Console.WriteLine("Iterate over the stack");
+
+            foreach (var stack in stacks)
+            {
+                Console.WriteLine(stack);
+            }
+
+
+        }
+    }
+}
