ca76b110598bf016d22a3c0ef47b0ee182783056
Neven Dimač <neven.dimac@gmail.com>
2022-07-14 10:09:47 +0200

    Add queue linked list implementation and test

---
+++ Queues/QueueLinkedList.cs
@@ -0,0 +1,46 @@
+﻿using Lists;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Queues
+{
+    public class QueueLinkedList<T> : IEnumerable<T>
+    {
+
+        private readonly SingleLinkedList<T> _list = new SingleLinkedList<T>();
+
+        public void Enqueue(T item)
+        {
+            _list.AddFirstElement(item);
+        }
+
+        public void Dequeue()
+        {
+            _list.RemoveFirstElement();
+        }
+
+        public T Peek()
+        {
+            if (IsEmpty)
+            {
+                throw new InvalidOperationException();
+            }
+            return _list.Head.Value;
+        }
+
+        public int Count => _list.Count;
+
+        public bool IsEmpty => Count == 0;
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return _list.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ QueuesTests/QueueLinkedList.cs
@@ -0,0 +1,64 @@
+﻿using NUnit.Framework;
+using Queues;
+using System;
+
+namespace QueuesTests
+{
+    [TestFixture]
+    public class QueueLinkedList
+    {
+
+        [Test]
+        public void IsEmpty_EmptyQueue_ReturnsTrue()
+        {
+            var queue = new QueueLinkedList<int>();
+            Assert.IsTrue(queue.IsEmpty);
+        }
+
+        [Test]
+        public void Count_EnqueueOneItem_ReturnsOne()
+        {
+            var queue = new QueueLinkedList<int>();
+            queue.Enqueue(1);
+
+            Assert.AreEqual(1, queue.Count);
+            Assert.IsFalse(queue.IsEmpty);
+
+        }
+
+
+        [Test]
+        public void Peek_EnqueuTwoElement_ReturnHeadElement()
+        {
+            var queue = new QueueLinkedList<int>();
+            queue.Enqueue(1);
+            queue.Enqueue(2);
+
+
+            Assert.AreEqual(1, queue.Peek());
+        }
+
+        [Test]
+        public void Peek_EnqueuTwoElementsAndDequeue_Return_HeadElement()
+        {
+            var queue = new QueueLinkedList<int>();
+            queue.Enqueue(1);
+            queue.Enqueue(2);
+
+            queue.Dequeue();
+
+            Assert.AreEqual(2, queue.Peek());
+        }
+
+
+        [Test]
+        public void Peek_EmptyStack_ThrowsException()
+        {
+            var queue = new QueueLinkedList<int>();
+            Assert.Throws<InvalidOperationException>(() =>
+            {
+                queue.Dequeue();
+            });
+        }
+    }
+}
