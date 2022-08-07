1dadb512183e5ab0e4e1911b8ef4957f3d71600f
Neven Dimač <neven.dimac@gmail.com>
2022-08-07 23:15:52 +0200

    Add hashtable project

---
+++ HashTables/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace HashTables
+{
+
+    /// <summary>
+    /// The Hashtable class represents a collection of key-and-value pairs that are organized based on the hash code of the key. 
+    /// It uses the key to access the elements in the collection
+    /// For new code, you shouldn't use non-generic collections: Instead of HashTable use Dictionary instead
+    /// --error prone
+    /// --less performant
+    /// </summary>
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
