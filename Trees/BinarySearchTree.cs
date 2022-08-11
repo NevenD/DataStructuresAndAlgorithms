63725898d6dde57c7edff8f2677e7c70c11635b5
Neven Dimač <neven.dimac@gmail.com>
2022-08-11 23:53:30 +0200

    Implemented binary search tree

--- Trees/BinarySearchTree.cs
+++ Trees/BinarySearchTree.cs
@@ -1,11 +1,60 @@
-﻿namespace Trees
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Trees
 {
-    /// <summary>
-    /// Each node has 0,1, or 2 children
-    /// Left and right child, each node in binary tree can have only 2 children
-    /// BST log(n) for insertion, deletion and retrival
-    /// </summary>
-    public class BinarySearchTree
+    public class BinarySearchTree<T> where T : IComparable<T>
     {
+        private TreeNode<T> _root;
+
+
+        public TreeNode<T> Get(T value)
+        {
+            return _root?.Get(value);
+        }
+
+        public T MinimumValue()
+        {
+            if (_root is null)
+            {
+                throw new InvalidOperationException();
+            }
+
+            return _root.MinimumValueTreeNode();
+        }
+
+        public T MaximumValue()
+        {
+            if (_root is null)
+            {
+                throw new InvalidOperationException();
+            }
+
+            return _root.MaximumValueTreeNode();
+        }
+
+        public void Insert(T value)
+        {
+            if (_root is null)
+            {
+                _root = new TreeNode<T>(value);
+            }
+            else
+            {
+                _root.Insert(value);
+            }
+        }
+
+        public IEnumerable<T> TraverseInOrder()
+        {
+            if (_root != null)
+            {
+                return _root.TraverseInOrder();
+            }
+
+            return Enumerable.Empty<T>();
+        }
+
     }
 }
--- Trees/Program.cs
+++ Trees/Program.cs
@@ -4,9 +4,33 @@ namespace Trees
 {
     public class Program
     {
+
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            var binarySearchTreeTest = new BinarySearchTree<int>();
+            binarySearchTreeTest.Insert(37);
+            binarySearchTreeTest.Insert(24);
+            binarySearchTreeTest.Insert(17);
+            binarySearchTreeTest.Insert(28);
+            binarySearchTreeTest.Insert(31);
+            binarySearchTreeTest.Insert(29);
+            binarySearchTreeTest.Insert(15);
+            binarySearchTreeTest.Insert(12);
+            binarySearchTreeTest.Insert(20);
+
+
+            foreach (var item in binarySearchTreeTest.TraverseInOrder())
+            {
+                Console.WriteLine($"{item}");
+
+            }
+            Console.WriteLine($"MIN: {binarySearchTreeTest.MinimumValue()}");
+            Console.WriteLine($"MAX: {binarySearchTreeTest.MaximumValue()}");
+            Console.WriteLine($"GET: {binarySearchTreeTest.Get(37).Value}");
+
+            Console.Read();
+
         }
     }
 }
