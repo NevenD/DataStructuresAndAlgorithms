5152eab8b1cf8b5c48ea42c7c46424a64f174c75
Neven Dimač <neven.dimac@gmail.com>
2022-08-22 19:36:24 +0200

    Implemented heap sort along with remove and peek method.

--- HeapAndHeapSort/Program.cs
+++ HeapAndHeapSort/Program.cs
@@ -7,12 +7,23 @@ namespace HeapAndHeapSort
         public static void Main(string[] args)
         {
 
-            var heapTest = new MaxHeap<int>();
+            var heapTest = new Heap<int>();
             heapTest.Insert(24);
             heapTest.Insert(37);
             heapTest.Insert(17);
             heapTest.Insert(28);
             heapTest.Insert(31);
+            heapTest.Insert(29);
+            heapTest.Insert(15);
+            heapTest.Insert(12);
+            heapTest.Insert(20);
+
+            Console.WriteLine(heapTest.Peek());
+            Console.WriteLine(heapTest.Remove());
+            Console.WriteLine(heapTest.Peek());
+            heapTest.Insert(40);
+            Console.WriteLine(heapTest.Peek());
+
 
             foreach (var item in heapTest.GetAllValues())
             {
