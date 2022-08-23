4f75fe49559e49c2c5ce76d79c639d3f29a2722f
Neven Dimač <neven.dimac@gmail.com>
2022-08-23 21:14:17 +0200

    Add concurent and immutable collections project

---
+++ ImmutableConcurrentCollections/ConcurrentCollections.cs
@@ -0,0 +1,15 @@
+﻿namespace ImmutableConcurrentCollections
+{
+
+    /// <summary>
+    /// Thread safe collections
+    /// NET Framework 4 introduced five collection types that are specially designed to support multi-threaded add and remove operations. 
+    /// To achieve thread-safety, these types use various kinds of efficient locking and lock-free synchronization mechanisms.
+    /// Synchronization adds overhead to an operation. 
+    /// The amount of overhead depends on the kind of synchronization that is used, the kind of operations that are performed, 
+    /// and other factors such as the number of threads that are trying to concurrently access the collection.
+    /// </summary>
+    public class ConcurrentCollections
+    {
+    }
+}
