78e34067808c2efc22951d8e9475014b02510adf
Neven Dimač <neven.dimac@gmail.com>
2022-08-23 20:52:51 +0200

    Added sieve of eratosten

--- AdditionalAlgorithms/Program.cs
+++ AdditionalAlgorithms/Program.cs
@@ -9,7 +9,14 @@ namespace AdditionalAlgorithms
         //https://github.com/TheAlgorithms/C-Sharp/blob/2362b6f8bcedfc995da1b8397f4f92939597fb29/Algorithms/Graph/Dijkstra/DijkstraAlgorithm.cs#L118
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            foreach (var prime in SieveOfEratosthenes.Sieve(30))
+            {
+                Console.WriteLine(prime);
+
+            }
+
+            Console.ReadLine();
         }
     }
 }
---
+++ AdditionalAlgorithms/SieveOfEratosthenes.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+
+namespace AdditionalAlgorithms
+{
+    /// <summary>
+    /// Generate sequence of prime numbers
+    /// Prime number is only divisible by one and itself (2,3,5,7,11,13,17, etc)
+    /// </summary>
+    public static class SieveOfEratosthenes
+    {
+
+        public static IEnumerable<int> Sieve(int max)
+        {
+            bool[] isPrimeNumber = new bool[max + 1];
+            byte minimumPrimeNumber = 2;
+
+            for (int prime = minimumPrimeNumber; prime <= max; prime++)
+            {
+
+                if (isPrimeNumber[prime])
+                {
+                    continue;
+                }
+
+                yield return prime;
+
+                for (int i = prime * prime; i <= max; i += prime)
+                {
+                    isPrimeNumber[i] = true;
+                }
+
+            }
+
+        }
+
+    }
+}
